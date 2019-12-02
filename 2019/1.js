76f9a581b0dc3b91415234a11af1ed4fc4efc6eb
estrandler <erik.strandler@tradera.com>
2019-12-02 16:55:39 +0100

    Solve 1b

--- 2019/1.js
+++ 2019/1.js
@@ -10,10 +10,17 @@ fs.readFile("1.txt", "utf8", (error, data) => {
     console.log('1a', solution);
 });
 
+const getFuelRecursive = (value, total) => {
+    const fuel = Math.floor(value / 3) - 2;
+    if(fuel < 1)
+        return total;
 
-fs.readFile("2.txt", "utf8", (error, data) => {
+    return getFuelRecursive(fuel, total + fuel);
+}
+
+fs.readFile("1.txt", "utf8", (error, data) => {
     const input = data.split('\r\n').map(val => parseInt(val, 10));
-    
-    //TODO
-});
+    const solution = input.reduce((prev, current) => getFuelRecursive(current, prev), 0);
 
+    console.log('1b', solution);
+});
