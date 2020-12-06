a73e23ce77ea77e04dacfd8c8f5dc15d47dd6b94
Erik Strandler <erik@strandler.se>
2020-12-06 12:21:27 +0100

    Golfing day 6

--- 2020/6.js
+++ 2020/6.js
@@ -18,18 +18,18 @@ const groups = fs
   );
 
 const A = groups
-  .map(({ text }) => String.prototype.concat(...new Set(text)))
-  .reduce((prev, curr) => prev + curr.length, 0);
+  .map(({ text }) => new Set(text))
+  .reduce((prev, curr) => prev + curr.size, 0);
 
 const B = groups
-  .map((group) => {
-    const all = String.prototype.concat(...new Set(group.text)).split("");
-    return all.filter(
-      (character) =>
-        group.text.split("").filter((groupChar) => groupChar === character)
-          .length === group.memberCount
-    ).length;
-  })
+  .map(
+    ({ text, memberCount }) =>
+      [...new Set(text)].filter(
+        (character) =>
+          text.split("").filter((groupChar) => groupChar === character)
+            .length === memberCount
+      ).length
+  )
   .reduce((prev, curr) => prev + curr, 0);
 
 console.log(A);
