07e0546c4420ad0cadb9701f1d8df692378b5be0
Erik Strandler <erik.strandler@earlybird.se>
2022-12-03 23:06:32 +0100

    2022 day 3 part 2

--- 2022/3.js
+++ 2022/3.js
@@ -67,3 +67,28 @@ const result = fs
   .reduce((prev, curr) => prev + map.indexOf(curr) + 1, 0);
 
 console.log("3a", result);
+
+const result2 = fs
+  .readFileSync("3.txt")
+  .toString()
+  .split("\r\n")
+  .reduce((prev, curr) => {
+    if (
+      prev.length === 0 ||
+      (prev[prev.length - 1] && prev[prev.length - 1].length === 3)
+    ) {
+      prev.push([]);
+    }
+    prev[prev.length - 1].push(curr);
+
+    return prev;
+  }, [])
+  .map((g) => ({
+    a: g[0].split(""),
+    b: g[1].split(""),
+    c: g[2].split(""),
+  }))
+  .map((r) => r.a.filter((l) => r.b.indexOf(l) > -1 && r.c.indexOf(l) > -1)[0])
+  .reduce((prev, curr) => prev + map.indexOf(curr) + 1, 0);
+
+console.log("3b", result2);
